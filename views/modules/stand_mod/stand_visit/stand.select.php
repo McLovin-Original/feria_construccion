034bcc6f30a5fe902ffa4435a97f094029e36ce7
Daniel Cardona <decardona34@misena.edu.co>
2017-09-05 19:34:13 -0500

    Stand select

--- views/modules/stand_mod/stand_visit/stand.select.php
+++ views/modules/stand_mod/stand_visit/stand.select.php
@@ -5,6 +5,11 @@
     if ($_SESSION["user"]["rol"]=="E3HDKX3684UTA7DMHFOAA34HAK39PM") {
       $code = $_SESSION["user"]["id"];
       $row = $this->StandM->readStandByUser($code);
+      ?>
+      <div class="selectconfe" style="width: 60%;">
+        <a href="stand-visit&token=<?php echo $row['sta_code']; ?>"><?php echo $row["sta_name"]; ?></a>
+      </div>
+      <?php
     }else{
       foreach ($this->StandM->readStand() as $row) {
       ?>
